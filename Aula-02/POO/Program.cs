9b27a2cae2aaa53afcce1ad765cebd3cb5892864
anny46712-spec <anny46712@gmail.com>
2026-07-29 16:40:02 -0300

    Aula 02 - Classe e Objeto

--- Aula-02/POO/Program.cs
+++ Aula-02/POO/Program.cs
@@ -1,1 +1,56 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Net.Http.Headers;
+using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+   // Comentário de Linha
+        /*
+           Comentário de Bloco
+           Pode ter várias linhas
+        */
+
+        // Escreve uma linha de Texto na tela
+        Console.WriteLine("Programação Orientada a Objetos\n");
+
+        Console.WriteLine("Abstração - 1° Pilar da POO");
+
+        /* Criar uma classe em C# 
+           A classe deve ser declarada fora do bloco de programa
+        */
+        
+        /*Criar um Objeto a partir de uma classe */
+
+         Pet pet1 = new Pet(); 
+         pet1.nome = "Totó";
+         pet1.tipo = "Cachorro";
+        pet1.idade = 3;
+        pet1.MostrarNome();
+
+         Pet pet2 = new Pet(); 
+         pet2.nome = "Mingau";
+         pet2.tipo = "Gato";
+        pet2.idade = 8;
+        pet2.MostrarNome();
+
+       
+       
+ }
+        public class Pet // Nome da classe sempre inicia com letras MAIUSCÚLA
+    {
+        // Declaração dos Atributos
+        public string nome;
+        public string tipo;
+        public int idade;
+
+        // Declaração dos Métodos
+        public void MostrarNome()
+        {
+            Console.WriteLine("Meu nome é " + this.nome);
+        }
+    }
+
+}
+
