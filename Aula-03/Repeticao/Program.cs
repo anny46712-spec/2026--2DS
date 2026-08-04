f8ab56102f9bf1cba89beced9825874eda1827fb
anny46712-spec <anny46712@gmail.com>
2026-08-04 14:59:19 -0300

    Função dos Vetores

--- Aula-03/Repeticao/Program.cs
+++ Aula-03/Repeticao/Program.cs
@@ -1,1 +1,68 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+using System.Net.Mail;
+using System.Runtime.CompilerServices;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*
+        sintaxe: Enquanto uma condição for verdadeira 
+
+        execute um conjunto de comandos
+
+        for -> Quando sabemos a quantidade de repetições.
+        while -> Quando a repetição depende de uma condição.
+        do ... while -> Quando precisa executar ao menos uma vez o bloco de codigo
+        */
+
+
+        /* Laço for: imprimir numeros de 1 até 10*/
+
+        Console.WriteLine("Imprimir numeros de 1 até 10, utilizando for.");
+        for(int numero = 1; numero <= 10; numero++ )
+        {
+            Console.WriteLine(numero);
+        }
+
+          /* Laço for: Contagem Regressiva de 10 até 0*/
+
+        Console.WriteLine("Imprimir Contagem Regressiva.");
+        for(int numero = 10; numero >= 0; numero-- )
+        {
+            Console.WriteLine(numero);
+        }
+
+        /* ********************************************** */
+        /* Laço whilw: imprimir numeros de 1 até 10*/
+        
+        Console.WriteLine("Imprimir numeros de 1 até 10, utilizando o laço while.");
+
+        int contador = 1;
+        while (contador <= 10)
+        {
+             Console.WriteLine(contador);
+            contador ++;
+        }
+
+          /* Solicitar a senha até acertar */
+        string senha = "";
+        while(senha != "")
+        {
+            Console.Write("Digite a Senha: ");
+            senha = Console.ReadLine();
+        }
+        Console.WriteLine("Acesso permitido!");
+        
+        /* ******************************************** */
+
+        int num;
+        do
+        {
+            Console.WriteLine("Digite um Numero Positivo: ");
+            num = int.Parse(Console.ReadLine());
+        }while(num <=0);
+
+
+    }
+}
---
+++ Aula-03/Vetores/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /* Vetores e Matrizes 
+        Vetor = é uma estrutura de dados utilizada para armazenar vários valores de um mesmo tipo dentro de uma única variável.
+
+        int[] nome vetor = new tipo[tamanho];
+        */
+
+        /* Iniciar um vetor -> lista de frutas */
+
+        string[] listaFrutas = { "Maçã", "Manga", "Morango", "Melancia", "Melão" };
+
+        Console.WriteLine("Acessar o terceiro elemento da lista:");
+        Console.WriteLine(listaFrutas[2]);
+
+
+        Console.WriteLine("Alterar o segundo elemento da lista:");
+        listaFrutas[1] = "Maçã";
+
+        for (int indice = 0; indice < listaFrutas.Length; indice++)
+        {
+            Console.WriteLine(listaFrutas[indice]);
+        }
+
+        /* *************************************************** */
+        foreach ( string fruta in listaFrutas)
+        {
+            Console.WriteLine(fruta);
+        }
+    }
+}
