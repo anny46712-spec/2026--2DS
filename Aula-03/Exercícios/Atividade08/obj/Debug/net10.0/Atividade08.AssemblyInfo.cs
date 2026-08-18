2f21d77e901133f8c37461a1fcfa1195bc01626c
anny46712-spec <anny46712@gmail.com>
2026-08-18 08:21:11 -0300

    Apply stash: resolve conflicts for build artifacts

--- Aula-03/Exercícios/Atividade08/obj/Debug/net10.0/Atividade08.AssemblyInfo.cs
+++ Aula-03/Exercícios/Atividade08/obj/Debug/net10.0/Atividade08.AssemblyInfo.cs
@@ -13,7 +13,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Atividade08")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ccb653b3d7f9fac2d8279f11a4dbbb8252a6789")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+090d9d53e08c12ccc565223f8c7972e4fcb6240b")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("Atividade08")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Atividade08")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
